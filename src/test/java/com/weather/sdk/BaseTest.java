cd8d51202ceaf472cb7ba6db2b857e95be340d40
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2025-02-20 14:07:16 +0300

    test: ensure WeatherSDK instances are cleared before tests

--- src/test/java/com/weather/sdk/BaseTest.java
+++ src/test/java/com/weather/sdk/BaseTest.java
@@ -2,6 +2,7 @@ package com.weather.sdk;
 
 import com.weather.sdk.enums.Mode;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,14 +15,21 @@ public abstract class BaseTest {
     @InjectMocks
     protected WeatherSDK weatherSDK;
 
+    @BeforeAll
+    static void beforeAll() {
+        WeatherSDK.clearInstances();
+    }
+
     @BeforeEach
     @DisplayName("Setup mock API key before each test")
     void setUp() {
+        WeatherSDK.clearInstances();
         weatherSDK = new WeatherSDK(Mode.ON_DEMAND);
     }
 
     @AfterEach
     void tearDown() {
         System.clearProperty("config.file");
+        WeatherSDK.clearInstances();
     }
 }
